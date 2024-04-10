5ba3018884888b0e36ce89a2a9db873a906f2086
gustavo oliveira <gugahb_10@hotmail.com>
2024-04-10 13:08:08 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/cursojava/course/entities/User.java
+++ src/main/java/com/cursojava/course/entities/User.java
@@ -2,12 +2,17 @@ package com.cursojava.course.entities;
 
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,6 +28,14 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
+	public List<Order> getOrders(){
+		return orders;
+	}
 	
 	public User() {
 		
